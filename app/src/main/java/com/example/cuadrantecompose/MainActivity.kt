2b4792f1b5b4dd0bcb55583c86b4853c5337e2da
JorgeBalmisa <131163333+JorgeBalmisa@users.noreply.github.com>
2023-10-18 20:32:51 +0200

    Update MainActivity.kt
    Terminado

--- app/src/main/java/com/example/cuadrantecompose/MainActivity.kt
+++ app/src/main/java/com/example/cuadrantecompose/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.cuadrantecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cuadrantecompose.ui.theme.CuadranteComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,21 +52,82 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
-private fun ArribaIzquierda(negrita : String, descripcion : String, modifier : Modifier = Modifier) {
-
+@Composable
+private fun ArribaIzquierda(negrita: String, descripcion: String, modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+        .background(Color(0xFFEADDFF))
+        .padding(16.dp)) {
+        Text(
+            text = negrita,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = descripcion,
+            textAlign = TextAlign.Justify,
+        )
+    }
 }
 
-private fun ArribaDerecha(negrita : String, descripcion : String, modifier : Modifier = Modifier) {
-
+@Composable
+private fun ArribaDerecha(negrita: String, descripcion: String, modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+        .background(Color(0xFFD0BCFF))
+        .padding(16.dp)) {
+        Text(
+            text = negrita,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Justify,
+        )
+        Text(
+            text = descripcion,
+            textAlign = TextAlign.Justify,
+        )
+    }
 }
 
-private fun AbajoIzquierda(negrita : String, descripcion : String, modifier : Modifier = Modifier) {
-
+@Composable
+private fun AbajoIzquierda(negrita: String, descripcion: String, modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+        .background(Color(0xFFB69DF8))
+        .padding(16.dp)) {
+        Text(
+            text = negrita,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Justify,
+        )
+        Text(
+            text = descripcion,
+            textAlign = TextAlign.Justify,
+        )
+    }
 }
-
-private fun ArribaDerecha(negrita : String, descripcion : String, modifier : Modifier = Modifier) {
-
+@Composable
+fun AbajoDerecha(negrita: String, descripcion: String, modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+        .background(Color(0xFFF6EDFF))
+        .padding(16.dp)) {
+        Text(
+            text = negrita,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Justify,
+        )
+        Text(
+            text = descripcion,
+            textAlign = TextAlign.Justify,
+        )
+    }
 }
 
 
@@ -58,6 +135,35 @@ private fun ArribaDerecha(negrita : String, descripcion : String, modifier : Mod
 @Composable
 fun GreetingPreview() {
     CuadranteComposeTheme {
-        Greeting("Android")
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    ArribaIzquierda("Text composable", stringResource(R.string.text_comp_desc))
+                }
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    ArribaDerecha("Image composable", stringResource(R.string.image_comp_desc))
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    AbajoIzquierda("Row composable", stringResource(R.string.row_comp_desc))
+                }
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    AbajoDerecha("Column composable", stringResource(R.string.col_comp_desc))
+                }
+            }
+        }
     }
 }
